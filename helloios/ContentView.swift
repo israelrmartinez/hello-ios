67ae62d675ebad801b14d060a2f1246fce51e0a5
israelrmartinez <israel.r.mar@gmail.com>
2022-04-05 20:57:11 -0700

    Initial Commit

---
+++ helloios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  helloios
+//
+//  Created by stlp on 4/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ helloios/helloiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloiosApp.swift
+//  helloios
+//
+//  Created by stlp on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct helloiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
